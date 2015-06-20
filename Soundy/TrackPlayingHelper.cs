c7761d348597bd484e80621732a0fbc05e1f7d4c
Mateusz Chodkowski <chotkos@gmail.com>
2015-06-21 01:21:29 +0200

    TrackPlayingHelper

--- Soundy/TrackPlayingHelper.cs
+++ Soundy/TrackPlayingHelper.cs
@@ -15,21 +15,47 @@ namespace Soundy
         // first define Cancellation Token Source - I've made it global so that CancelButton has acces to it
         private CancellationTokenSource cts = new CancellationTokenSource();
         private enum Problem { Ok, Cancelled, Other }; // results of my Task
-
+        private Track _track { get; set; }
+        private MediaElement _playbackElement { get; set; }
+        private int nowPlaying = 0;
         public TrackPlayingHelper(Track track, MediaElement playbackElement)
         {
-            HttpClient httpc = new HttpClient();
+            _track = track;
+            _playbackElement = playbackElement;
+            _playbackElement.MediaEnded +=_playbackElement_MediaEnded;
+            _playbackElement.MediaFailed += _playbackElement_MediaFailed;
+        }
 
-            foreach (var item in track.Sounds)
+        private void _playbackElement_MediaFailed(object sender, Windows.UI.Xaml.ExceptionRoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void _playbackElement_MediaEnded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            nowPlaying++;
+            if (nowPlaying < _track.Sounds.Count)
             {
-                httpc = new HttpClient();
-                httpc.BaseAddress = new Uri(item.FilePath);
-                //var res = httpc.GetAsync();
+                var sound = _track.Sounds.Single(x => x.StepNumber == nowPlaying);
+                _playbackElement.Source = new Uri(sound.FilePath, UriKind.RelativeOrAbsolute);
 
+                _playbackElement.AutoPlay = true;
+                _playbackElement.Play();
             }
         }
 
 
+
+        public void playTrack() {
+            if (_track.Sounds.Any())
+            {
+                var sound = _track.Sounds.OrderBy(x => x.StepNumber).First();
+                _playbackElement.Source = new Uri(sound.FilePath, UriKind.RelativeOrAbsolute);
+
+                _playbackElement.AutoPlay = true;
+                _playbackElement.Play();
+            }
+        }
 
     }
 }
