ce60b5c79ba1c06d02fb550419ed4c4883eb4d22
Mateusz Chodkowski <chotkos@gmail.com>
2015-06-20 22:42:12 +0200

    Nagrywanie dźwięku, prowizorka jak cholera

---
+++ Soundy/BasicPage1.xaml.cs
@@ -0,0 +1,232 @@
+﻿using Soundy.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Media;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace Soundy
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BasicPage1 : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        //Kokosowe metody
+        //WAŻNE KURWA RZECZY
+        private MediaCapture _mediaCaptureManager;
+        private StorageFile _recordStorageFile;
+        private bool _recording;
+        private bool _userRequestedRaw;
+        private bool _rawAudioSupported;
+        private DispatcherTimer _dispatcherTimer = new DispatcherTimer();
+
+        public BasicPage1()
+        {
+            this.InitializeComponent();
+
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+
+            //Kokosowe metody
+            _dispatcherTimer.Tick += _dispatcherTimer_Tick;
+            InitializeAudioRecording();
+        }
+
+
+        /// <summary>
+        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// Gets the view model for this <see cref="Page"/>.
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// <summary>
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// <para>
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="NavigationHelper.LoadState"/>
+        /// and <see cref="NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+        /// </para>
+        /// </summary>
+        /// <param name="e">Provides data for navigation methods and event
+        /// handlers that cannot cancel the navigation request.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+
+#region KOKOSOWE METODY
+        private async void InitializeAudioRecording()
+        {
+            _mediaCaptureManager = new MediaCapture();
+            var settings = new MediaCaptureInitializationSettings();
+            settings.StreamingCaptureMode = StreamingCaptureMode.Audio;
+            settings.MediaCategory = MediaCategory.Other;
+            settings.AudioProcessing = (_rawAudioSupported && _userRequestedRaw) ? AudioProcessing.Raw : AudioProcessing.Default;
+
+            await _mediaCaptureManager.InitializeAsync(settings);
+
+            Debug.WriteLine("Device initialised successfully");
+
+            _mediaCaptureManager.RecordLimitationExceeded += new RecordLimitationExceededEventHandler(failedToRec);
+            _mediaCaptureManager.Failed += new MediaCaptureFailedEventHandler(failedToRec);
+        }
+        private void failedToRec(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            //throw new NotImplementedException();
+            Debug.WriteLine("Device failed to rec");
+        }
+        private void failedToRec(MediaCapture sender)
+        {
+            //throw new NotImplementedException();
+            Debug.WriteLine("Device failed to rec2");
+        }
+        private async void CaptureAudio()
+        {
+            try
+            {
+                Debug.WriteLine("Starting record");
+                String fileName = "record.m4a";
+
+                _recordStorageFile = await KnownFolders.VideosLibrary.CreateFileAsync(fileName, CreationCollisionOption.GenerateUniqueName);
+
+                Debug.WriteLine("Create record file successfully");
+
+                MediaEncodingProfile recordProfile = MediaEncodingProfile.CreateM4a(AudioEncodingQuality.Auto);
+                await _mediaCaptureManager.StartRecordToStorageFileAsync(recordProfile, this._recordStorageFile);
+
+                Debug.WriteLine("Start Record successful");
+
+                _recording = true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Failed to capture audio");
+            }
+        }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            StopTimer();
+        }
+        private async void StopTimer()
+        {
+            if (_recording)
+            {
+                Debug.WriteLine("Stopping recording");
+                await _mediaCaptureManager.StopRecordAsync();
+                Debug.WriteLine("Stop recording successful");
+                _recording = false;
+            }
+        }
+        private void CaptureButton_Click(object sender, RoutedEventArgs e)
+        {
+            _dispatcherTimer.Interval = new TimeSpan(0, 0, 3);
+            _dispatcherTimer.Start();
+            CaptureAudio();
+        }
+
+
+        void _dispatcherTimer_Tick(object sender, object e)
+        {
+            StopTimer();
+            _dispatcherTimer.Stop();
+            _recording = false;
+        }
+
+        private async void playRecorded_Click(object sender, RoutedEventArgs e)
+        {
+
+            //UWAGA! jak będzie model to trzeba po kolei odtworzyć wszystkie w kolejności!
+            if (!_recording)
+            {
+                var stream = await _recordStorageFile.OpenAsync(FileAccessMode.Read); 
+
+                Debug.WriteLine("Recording file opened");
+                playbackElement1.AutoPlay = true;
+                playbackElement1.SetSource(stream, _recordStorageFile.FileType);
+                playbackElement1.Play();
+            }
+        }
+
+#endregion
+
+        private void removeRecorded_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
