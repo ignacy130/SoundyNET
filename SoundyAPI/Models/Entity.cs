835c9d71f36d0b493efa45a135f31bfba42d1485
unknown <ignacy130@gmail.com>
2015-06-20 23:20:36 +0200

    proper entity model

--- SoundyAPI/Models/Entity.cs
+++ SoundyAPI/Models/Entity.cs
@@ -8,5 +8,8 @@ namespace SoundyAPI.Models
     public abstract class Entity
     {
         public virtual Guid Id { get; set; }
+        public virtual DateTime Created { get; set; }
+        public virtual DateTime Modified { get; set; }
+        public virtual bool Deleted { get; set; }
     }
 }
