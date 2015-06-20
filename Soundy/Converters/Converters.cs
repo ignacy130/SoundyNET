6a6014ae4a83df80f50e18d8be2b3de3c0733786
unknown <ignacy130@gmail.com>
2015-06-20 22:45:23 +0200

    main view

---
+++ Soundy/Converters/Converters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace Soundy.Converters
+{
+
+    public class PlayPauseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool isPlaying = (bool)value;
+            return isPlaying ? new SymbolIcon(Symbol.Pause) : new SymbolIcon(Symbol.Play);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Soundy/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soundy.ViewModels
+{
+    public class MainPageViewModel : ObservableObject
+    {
+        public ObservableCollection<string> Friends { get; set; }
+        public ObservableCollection<string> Soundies { get; set; }
+
+        public MainPageViewModel()
+        {
+            this.Friends = new ObservableCollection<string>(){"Jan", "Tomek"};
+            this.Soundies = new ObservableCollection<string>() { "Soudn1", "Sound2" };
+        }
+
+        public bool IsRecording { get; set; }
+
+        public RelayCommand<bool> ToggleRecord{
+            get
+            {
+                return new RelayCommand<bool>(
+                    (obj) =>
+                    {
+                        this.IsRecording = !this.IsRecording;
+                    });
+            }
+    }
+    }
+}
