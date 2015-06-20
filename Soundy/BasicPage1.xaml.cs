63a41d6d60776172571af4a7736ab7d839087ac2
Mateusz Chodkowski <chotkos@gmail.com>
2015-06-20 23:07:34 +0200

    SoundRecordingHelper

--- Soundy/BasicPage1.xaml.cs
+++ Soundy/BasicPage1.xaml.cs
@@ -33,15 +33,7 @@ namespace Soundy
     {
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
-
-        //Kokosowe metody
-        //WAŻNE KURWA RZECZY
-        private MediaCapture _mediaCaptureManager;
-        private StorageFile _recordStorageFile;
-        private bool _recording;
-        private bool _userRequestedRaw;
-        private bool _rawAudioSupported;
-        private DispatcherTimer _dispatcherTimer = new DispatcherTimer();
+        private SoundRecordingHelper soundRecordingHelper = new SoundRecordingHelper();
 
         public BasicPage1()
         {
@@ -52,8 +44,6 @@ namespace Soundy
             this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
 
             //Kokosowe metody
-            _dispatcherTimer.Tick += _dispatcherTimer_Tick;
-            InitializeAudioRecording();
         }
 
 
@@ -128,105 +118,27 @@ namespace Soundy
 
         #endregion
 
-
-#region KOKOSOWE METODY
-        private async void InitializeAudioRecording()
-        {
-            _mediaCaptureManager = new MediaCapture();
-            var settings = new MediaCaptureInitializationSettings();
-            settings.StreamingCaptureMode = StreamingCaptureMode.Audio;
-            settings.MediaCategory = MediaCategory.Other;
-            settings.AudioProcessing = (_rawAudioSupported && _userRequestedRaw) ? AudioProcessing.Raw : AudioProcessing.Default;
-
-            await _mediaCaptureManager.InitializeAsync(settings);
-
-            Debug.WriteLine("Device initialised successfully");
-
-            _mediaCaptureManager.RecordLimitationExceeded += new RecordLimitationExceededEventHandler(failedToRec);
-            _mediaCaptureManager.Failed += new MediaCaptureFailedEventHandler(failedToRec);
-        }
-        private void failedToRec(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
-        {
-            //throw new NotImplementedException();
-            Debug.WriteLine("Device failed to rec");
-        }
-        private void failedToRec(MediaCapture sender)
-        {
-            //throw new NotImplementedException();
-            Debug.WriteLine("Device failed to rec2");
-        }
-        private async void CaptureAudio()
-        {
-            try
-            {
-                Debug.WriteLine("Starting record");
-                String fileName = "record.m4a";
-
-                _recordStorageFile = await KnownFolders.VideosLibrary.CreateFileAsync(fileName, CreationCollisionOption.GenerateUniqueName);
-
-                Debug.WriteLine("Create record file successfully");
-
-                MediaEncodingProfile recordProfile = MediaEncodingProfile.CreateM4a(AudioEncodingQuality.Auto);
-                await _mediaCaptureManager.StartRecordToStorageFileAsync(recordProfile, this._recordStorageFile);
-
-                Debug.WriteLine("Start Record successful");
-
-                _recording = true;
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("Failed to capture audio");
-            }
-        }
-        private void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            StopTimer();
-        }
-        private async void StopTimer()
-        {
-            if (_recording)
-            {
-                Debug.WriteLine("Stopping recording");
-                await _mediaCaptureManager.StopRecordAsync();
-                Debug.WriteLine("Stop recording successful");
-                _recording = false;
-            }
-        }
-        private void CaptureButton_Click(object sender, RoutedEventArgs e)
-        {
-            _dispatcherTimer.Interval = new TimeSpan(0, 0, 3);
-            _dispatcherTimer.Start();
-            CaptureAudio();
-        }
-
-
-        void _dispatcherTimer_Tick(object sender, object e)
-        {
-            StopTimer();
-            _dispatcherTimer.Stop();
-            _recording = false;
-        }
-
         private async void playRecorded_Click(object sender, RoutedEventArgs e)
         {
-
             //UWAGA! jak będzie model to trzeba po kolei odtworzyć wszystkie w kolejności!
-            if (!_recording)
+            if (!soundRecordingHelper._recording)
             {
-                var stream = await _recordStorageFile.OpenAsync(FileAccessMode.Read); 
-
+                var stream = await soundRecordingHelper._recordStorageFile.OpenAsync(FileAccessMode.Read);
                 Debug.WriteLine("Recording file opened");
                 playbackElement1.AutoPlay = true;
-                playbackElement1.SetSource(stream, _recordStorageFile.FileType);
+                playbackElement1.SetSource(stream, soundRecordingHelper._recordStorageFile.FileType);
                 playbackElement1.Play();
             }
         }
 
-#endregion
+        private void CaptureButton_Click(object sender, RoutedEventArgs e)
+        {
+            soundRecordingHelper.CaptureSound();
+        }
 
         private void removeRecorded_Click(object sender, RoutedEventArgs e)
         {
-
+            //Zabrać baterię! Schować Guzik od nagywania! Napis: jeszcze raz czy coś takiego!
         }
     }
 }
