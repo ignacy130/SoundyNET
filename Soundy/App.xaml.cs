779b7dd819ff04436cc0175006a8292c7566b692
Mateusz Chodkowski <chotkos@gmail.com>
2015-06-21 07:36:07 +0200

    Logowanie/rejestracja usera

--- Soundy/App.xaml.cs
+++ Soundy/App.xaml.cs
@@ -97,7 +97,7 @@ namespace Soundy
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
+                if (!rootFrame.Navigate(typeof(RegisterPage), e.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
